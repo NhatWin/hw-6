b7435ad81d3dd3ebad8a4a3032afdcd3387ea35f
NhatWin <nhat0933@gmail.com>
2022-11-10 19:12:55 -0600

    added TODO list

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-console.log("hello");
+const cityCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=1866bb0371e7ecff1990b7e071a75947`
+
+const cityWeather = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1866bb0371e7ecff1990b7e071a75947`
+
+// TODO: make search bar send request to fetch weather information based on city given
+
+// TODO: get the lat and log from a given city
+
+// TODO: use lat and long to get weather infromation on a specific place
+
+// TODO: display weather information on main screen
+
+// TODO: display weather information for upcoming 5 days below main
+
+//TODO: display previous searches below search bar
