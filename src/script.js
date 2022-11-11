2b3456abf25789d75b5fe873a1c3d44aa2d53f17
NhatWin <nhat0933@gmail.com>
2022-11-10 23:21:40 -0600

    Made Search bar fetch city corrdinates

--- src/script.js
+++ src/script.js
@@ -1,12 +1,25 @@
-const cityCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname}&limit=1&appid=1866bb0371e7ecff1990b7e071a75947`
+// const cityWeather = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1866bb0371e7ecff1990b7e071a75947`
+const cityCoordinates = {};
+const search = document.querySelector("#form");
 
-const cityWeather = `api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=1866bb0371e7ecff1990b7e071a75947`
+search.addEventListener("submit", function (event) {
+  event.preventDefault();
+  cityCoordinates.city = `http://api.openweathermap.org/geo/1.0/direct?q=${event.target.city.value}&limit=1&appid=1866bb0371e7ecff1990b7e071a75947`
+  getCoordinates()
+});
 
-// TODO: make search bar send request to fetch weather information based on city given
+function getCoordinates() {
+  fetch(cityCoordinates.city)
+    .then(function (weather) {
+      return weather.json();
+    })
+    .then(function (data) {
+      const cityWeather = `api.openweathermap.org/data/2.5/forecast?lat=${data[0].lat}&lon=${data[0].lon}&appid=1866bb0371e7ecff1990b7e071a75947`
+      console.log(cityWeather)
+    })
+}
 
-// TODO: get the lat and log from a given city
-
-// TODO: use lat and long to get weather infromation on a specific place
+// TODO: use lat and lon to get weather infromation on a specific place
 
 // TODO: display weather information on main screen
 
