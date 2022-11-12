c16041523b91619f6de4c6f66adeb573f027823d
NhatWin <nhat0933@gmail.com>
2022-11-11 20:00:05 -0600

    Added search history bar

--- src/script.js
+++ src/script.js
@@ -1,11 +1,15 @@
 const cityWeather = {};
 const cityCoordinates = {};
 const search = document.querySelector("#form");
+const ul = document.querySelector("#search-history");
 
 search.addEventListener("submit", function (event) {
   event.preventDefault();
   cityCoordinates.data = `http://api.openweathermap.org/geo/1.0/direct?q=${event.target.city.value}&limit=1&appid=1866bb0371e7ecff1990b7e071a75947`
-  getWeather()
+  getWeather();
+  const li = document.createElement("li");
+  li.textContent = event.target.city.value
+  ul.append(li);
 });
 
 function getWeather() {
@@ -26,7 +30,7 @@ function getWeather() {
           // date
           console.log(data.list[0].dt_txt);
           //image
-          console.log(data.list[0].weather[0].id);
+          console.log(data.list[0].weather[0].icon);
           // temp
           console.log(data.list[0].main.temp);
           // wind
@@ -35,7 +39,10 @@ function getWeather() {
     })
 }
 
-// TODO: use lat and lon to get weather infromation on a specific place
+function addHistory() {
+  const li = document.createElement("li");
+  li.textContent = event.target.city.value
+}
 
 // TODO: display weather information on main screen
 
